77651d14b5fd0fee75a86d8729603f92bf0e839a
jesus-diaz <jesus@jdiaz.com>
2025-08-12 19:18:22 -0600

    Add Grafana Faro SDK for monitoring and tracing
    - Updated package.json to include @grafana/faro-web-sdk and @grafana/faro-web-tracing dependencies.
    - Enhanced app.component.ts to initialize Faro for application monitoring and added tracing instrumentation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,29 @@ import { HomeComponent } from './pages/home/home.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // CORRECTO: styleUrls en plural
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'artisana';
+
+  ngOnInit(): void {
+
+    initializeFaro({
+      url: 'https://faro-collector-prod-us-west-0.grafana.net/collect/ae343da272d3a2d5766685b60487920d',
+      app: {
+        name: 'artisana',
+        version: '1.0.0',
+        environment: 'production'
+      },
+
+      instrumentations: [
+        // Mandatory, omits default instrumentations otherwise.
+        ...getWebInstrumentations(),
+
+        // Tracing package to get end-to-end visibility for HTTP requests.
+        new TracingInstrumentation(),
+      ],
+    });
+
+  }
+
 }
